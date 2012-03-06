1f76592e562859ad3541a521a49990ec25dc7b0c
Eric Blair <sexndrugsnsourcecode@gmail.com>
2012-03-06 17:33:19 +0000

    Created CalculateLastBusinessDay class as well as the IRepository interface (so project would build)

---
+++ OfflineSiteMonitoringTool.Model/CalculateLastBusinessDay.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OfflineSiteMonitoringTool.Model
+{
+    public class CalculateLastBusinessDay
+    {
+        private IRepository _repository;
+        private DateTime _currentDate;
+
+        public CalculateLastBusinessDay(IRepository repository, DateTime currentDate)
+        {
+            _repository = repository;
+            _currentDate = currentDate;
+
+            setLastBusinessDay();
+        }
+        private DateTime lastBusinessDay;
+
+        private void setLastBusinessDay()
+        {
+            DateTime currentDate = _currentDate;
+            lastBusinessDay = _repository.GetLastBusinessDay(currentDate);
+        }
+
+        public DateTime GetLastBusinessDay { get { return lastBusinessDay; } }
+    }
+}
---
+++ OfflineSiteMonitoringTool.Model/IRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Pex.Framework;
+
+namespace OfflineSiteMonitoringTool.Model
+{
+    public interface IRepository
+    {
+        DateTime GetLastBusinessDay(DateTime currentDate);
+        Boolean HasDataBeenUpdatedSinceLastBusinessDay(DateTime lastBusinessDay);
+    }
+}
