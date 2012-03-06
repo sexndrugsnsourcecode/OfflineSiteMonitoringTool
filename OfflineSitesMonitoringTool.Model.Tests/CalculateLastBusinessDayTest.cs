736361de679f2e2ae8239cb78ad8e24e0671f5b9
Eric Blair <sexndrugsnsourcecode@gmail.com>
2012-03-06 17:42:08 +0000

    Completed unit test for CalculateLastBusinessDay

--- OfflineSitesMonitoringTool.Model.Tests/CalculateLastBusinessDayTest.cs
+++ OfflineSitesMonitoringTool.Model.Tests/CalculateLastBusinessDayTest.cs
@@ -1,81 +1,33 @@
 ﻿using OfflineSiteMonitoringTool.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Moq;
 
 namespace OfflineSitesMonitoringTool.Model.Tests
 {
-    
-    
-    /// <summary>
-    ///This is a test class for CalculateLastBusinessDayTest and is intended
-    ///to contain all CalculateLastBusinessDayTest Unit Tests
-    ///</summary>
-    [TestClass()]
+    [TestClass]
     public class CalculateLastBusinessDayTest
     {
-
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        // 
-        //You can use the following additional attributes as you write your tests:
-        //
-        //Use ClassInitialize to run code before running the first test in the class
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext)
-        //{
-        //}
-        //
-        //Use ClassCleanup to run code after all tests in a class have run
-        //[ClassCleanup()]
-        //public static void MyClassCleanup()
-        //{
-        //}
-        //
-        //Use TestInitialize to run code before running each test
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{
-        //}
-        //
-        //Use TestCleanup to run code after each test has run
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{
-        //}
-        //
-        #endregion
-
-
         /// <summary>
         ///A test for GetLastBusinessDay
         ///</summary>
-        [TestMethod()]
-        public void GetLastBusinessDayTest()
+        [TestMethod]
+        public void GetLastBusinessDay_ReturnsLastBusinessDay()
         {
-            IRepository repository = null; // TODO: Initialize to an appropriate value
-            DateTime currentDate = new DateTime(); // TODO: Initialize to an appropriate value
-            CalculateLastBusinessDay target = new CalculateLastBusinessDay(repository, currentDate); // TODO: Initialize to an appropriate value
-            DateTime actual;
-            actual = target.GetLastBusinessDay;
-            Assert.Inconclusive("Verify the correctness of this test method.");
+            Mock<IRepository> repositoryMock = new Mock<IRepository>(MockBehavior.Strict);
+            IRepository repository = repositoryMock.Object;
+            DateTime currentDate = new DateTime(2012, 1, 2);
+            DateTime lastBusinessDay = new DateTime(2012, 1, 1);
+
+            // Setup repository to return lastBusinessDay
+            repositoryMock.Setup(x => x.GetLastBusinessDay(currentDate)).Returns(lastBusinessDay);
+
+            CalculateLastBusinessDay calculateLastBusinessDay = new CalculateLastBusinessDay(repository, currentDate);
+
+            DateTime result;
+            result = calculateLastBusinessDay.GetLastBusinessDay;
+
+            Assert.AreEqual(lastBusinessDay, result);
         }
     }
 }
