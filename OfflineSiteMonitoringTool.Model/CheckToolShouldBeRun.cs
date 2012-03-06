692495cab83c26b07d84d0707704dfa10f7fc432
Eric Blair <sexndrugsnsourcecode@gmail.com>
2012-03-06 17:52:31 +0000

    Added CheckToolShouldBeRun class and tests (forgot to create branch...)

---
+++ OfflineSiteMonitoringTool.Model/CheckToolShouldBeRun.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OfflineSiteMonitoringTool.Model
+{
+    public class CheckToolShouldBeRun
+    {
+        private IRepository _repository;
+        private DateTime _lastBusinessDay;
+
+        public CheckToolShouldBeRun(IRepository repository, DateTime lastBusinessDay)
+        {
+            _repository = repository;
+            _lastBusinessDay = lastBusinessDay;
+
+            checkDataHasBeenUpdatedSinceLastBusinessDay();
+
+            if (hasDataBeenUpdatedSinceLastBusinessDay == true)
+                shouldToolBeRun = true;
+            else
+                shouldToolBeRun = false;
+        }
+
+        private bool hasDataBeenUpdatedSinceLastBusinessDay;
+
+        private void checkDataHasBeenUpdatedSinceLastBusinessDay()
+        {
+            hasDataBeenUpdatedSinceLastBusinessDay = _repository.HasDataBeenUpdatedSinceLastBusinessDay(_lastBusinessDay);
+        }
+
+        private bool shouldToolBeRun = false;
+
+        public bool ShouldToolBeRun { get { return shouldToolBeRun; } }
+    }
+}
---
+++ OfflineSitesMonitoringTool.Model.Tests/CheckToolShouldBeRunTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using OfflineSiteMonitoringTool.Model;
+
+namespace OfflineSitesMonitoringTool.Model.Tests
+{
+    /// <summary>
+    /// Summary description for CheckToolShouldBeRunTests
+    /// </summary>
+    [TestClass]
+    public class CheckToolShouldBeRunTests
+    {
+        [TestMethod]
+        public void ShouldToolBeRun_ReportingDataHasBeenUpdatedSinceLastBusinessDay_ReturnsTrue()
+        {
+            Mock<IRepository> repositoryMock = new Mock<IRepository>(MockBehavior.Strict);
+            IRepository repository = repositoryMock.Object;
+            DateTime lastBusinessDay = new DateTime();
+
+            // Setup mock to look like reporting data has been updated
+            repositoryMock.Setup(x => x.HasDataBeenUpdatedSinceLastBusinessDay(lastBusinessDay)).Returns(true);
+
+            CheckToolShouldBeRun checkToolShouldBeRun = new CheckToolShouldBeRun(repository, lastBusinessDay);
+
+            bool result;
+            result = checkToolShouldBeRun.ShouldToolBeRun;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ShouldToolBeRun_ReportingDataHasNotBeenUpdatedSinceLastBusinessDay_ReturnsFalse()
+        {
+            Mock<IRepository> repositoryMock = new Mock<IRepository>(MockBehavior.Strict);
+            IRepository repository = repositoryMock.Object;
+            DateTime lastBusinessDay = new DateTime();
+
+            // Setup mock to look like reporting data has not been updated
+            repositoryMock.Setup(x => x.HasDataBeenUpdatedSinceLastBusinessDay(lastBusinessDay)).Returns(false);
+
+            CheckToolShouldBeRun checkToolShouldBeRun = new CheckToolShouldBeRun(repository, lastBusinessDay);
+
+            bool result;
+            result = checkToolShouldBeRun.ShouldToolBeRun;
+
+            Assert.IsFalse(result);
+        }
+    }
+}
