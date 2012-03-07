8f455a7bf563c6776d3f09ba5938cb8dcf995f5f
Eric Blair <sexndrugsnsourcecode@gmail.com>
2012-03-07 21:23:43 +0000

    Added skeleton UpdateOfflineSitesData class and test templates

---
+++ OfflineSiteMonitoringTool.Model/UpdateOfflineSiteData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OfflineSiteMonitoringTool.Model
+{
+    public class UpdateOfflineSiteData
+    {
+        private IRepository _repository;
+
+        private List<string> _offlineSites;
+        private List<string> _sitesRecordedAsOffline;
+        
+        public UpdateOfflineSiteData(IRepository repository, IList<string> offlineSites)
+        {
+            _repository = repository;
+
+            _sitesRecordedAsOffline = getSitesRecordedAsOffline();
+
+
+        }
+
+        
+
+        private List<string> getSitesRecordedAsOffline()
+        {
+            List<string> sitesRecordedAsOffline = new List<string>();
+
+            //sitesRecordedAsOffline = _repository.GetSitesRecordedAsOffline();
+
+            return sitesRecordedAsOffline;
+        }
+
+        public void RemoveOnlineSites(List<string> sitesRecordedAsOffline, List<string> offlineSites)
+        {
+            // remove sites that have come online from offline site table
+
+            // for all sites present in sitesRecordedAsOffline but not in offlineSites, remove those sites from table
+        }
+
+        public void UpdateSitesAlreadyRecordedAsOffline(List<string> sitesRecordedAsOffline, List<string> offlineSites)
+        {
+            // update any offline sites that are already recorded
+
+            // for all sites in sitesRecordedAsOffline and offlineSites, update the site's row in table
+        }
+        
+        public void RecordNewOfflineSites(List<string> sitesRecordedAsOffline, List<string> offlineSites)
+        {
+            // record any offline sites not already recorded
+
+            // for all sites in sitesRecordedAsOffline but not in offlineSites, add details of site to table
+        }
+        
+    }
+}
---
+++ OfflineSitesMonitoringTool.Model.Tests/UpdateOfflineSiteDataTest.cs
@@ -0,0 +1,113 @@
+﻿using OfflineSiteMonitoringTool.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace OfflineSitesMonitoringTool.Model.Tests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for UpdateOfflineSiteDataTest and is intended
+    ///to contain all UpdateOfflineSiteDataTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class UpdateOfflineSiteDataTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for RecordNewOfflineSites
+        ///</summary>
+        [TestMethod()]
+        public void RecordNewOfflineSitesTest()
+        {
+            IRepository repository = null; // TODO: Initialize to an appropriate value
+            IList<string> offlineSites = null; // TODO: Initialize to an appropriate value
+            UpdateOfflineSiteData target = new UpdateOfflineSiteData(repository, offlineSites); // TODO: Initialize to an appropriate value
+            List<string> sitesRecordedAsOffline = null; // TODO: Initialize to an appropriate value
+            List<string> offlineSites1 = null; // TODO: Initialize to an appropriate value
+            target.RecordNewOfflineSites(sitesRecordedAsOffline, offlineSites1);
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+
+        /// <summary>
+        ///A test for RemoveOnlineSites
+        ///</summary>
+        [TestMethod()]
+        public void RemoveOnlineSitesTest()
+        {
+            IRepository repository = null; // TODO: Initialize to an appropriate value
+            IList<string> offlineSites = null; // TODO: Initialize to an appropriate value
+            UpdateOfflineSiteData target = new UpdateOfflineSiteData(repository, offlineSites); // TODO: Initialize to an appropriate value
+            List<string> sitesRecordedAsOffline = null; // TODO: Initialize to an appropriate value
+            List<string> offlineSites1 = null; // TODO: Initialize to an appropriate value
+            target.RemoveOnlineSites(sitesRecordedAsOffline, offlineSites1);
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+
+        /// <summary>
+        ///A test for UpdateSitesAlreadyRecordedAsOffline
+        ///</summary>
+        [TestMethod()]
+        public void UpdateSitesAlreadyRecordedAsOfflineTest()
+        {
+            IRepository repository = null; // TODO: Initialize to an appropriate value
+            IList<string> offlineSites = null; // TODO: Initialize to an appropriate value
+            UpdateOfflineSiteData target = new UpdateOfflineSiteData(repository, offlineSites); // TODO: Initialize to an appropriate value
+            List<string> sitesRecordedAsOffline = null; // TODO: Initialize to an appropriate value
+            List<string> offlineSites1 = null; // TODO: Initialize to an appropriate value
+            target.UpdateSitesAlreadyRecordedAsOffline(sitesRecordedAsOffline, offlineSites1);
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+    }
+}
