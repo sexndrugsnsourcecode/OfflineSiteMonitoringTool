26a0f1bae731a484a8e2e90bdff785e1897e3719
Eric Blair <sexndrugsnsourcecode@gmail.com>
2012-03-13 17:42:36 +0000

    Implemented a skeleton version of the Repository class

---
+++ OfflineSiteMonitoringTool.Model/IConfigHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OfflineSiteMonitoringTool.Model
+{
+    public interface IConfigHelper
+    {
+        //Int16 GetHealthboardOfflineOrganisationsLimit();
+        //Int16 GetNumberOfRetriesAfterDatabaseError();
+        //string GetFromAddress();
+        //string GetReplyToAddress();
+    }
+}
---
+++ OfflineSiteMonitoringTool.Repository/Repository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using OfflineSiteMonitoringTool.Model;
+
+namespace OfflineSiteMonitoringTool.Repository
+{
+    public class Repository : IRepository
+    {
+        // Added dummy implementations of each method required by interface here to get solution to build
+        // will remove these methods into their own file as I work through them
+        public DateTime GetLastBusinessDay(DateTime currentDate) { return new DateTime(); }
+        public Boolean HasDataBeenUpdatedSinceLastBusinessDay(DateTime lastBusinessDay) { return false; }
+        public List<string> GetSitesToCheckMessagingActivityFor() { return new List<string>(); }
+        public List<string> GetOfflineSites(List<string> sitesToCheckMessagingActivityFor, DateTime lastBusinessDay) { return new List<string>(); }
+        public List<string> GetSitesRecordedAsOffline() { return new List<string>(); }
+        public void RemoveOnlineSite(string onlineSite) { }
+        public void UpdateSiteAlreadyRecordedAsOffline(string offlineSite) { }
+        public void RecordNewOfflineSite(string offlineSite) { }
+        public List<string> GetSuppliersToReceiveOfflineNotifications() { return new List<string>(); }
+        public int GetNumberOfOfflineSitesToBeReportedPerHealthboardLimit() { return 0; }
+        public List<string> GetHealthboardsThatHaveExceededNumberOfOfflineSitesToBeReportedLimit(int numberOfOfflineSitesToBeReportedPerHealthboardLimit) { return new List<string>(); }
+        public List<SiteDetails> GetOfflineSitesToReport(List<string> healthboardsThatHaveExceededNumberOfOfflineSitesToBeReportedLimit) { return new List<SiteDetails>(); }
+        public MailAddress GetOfflineReportFromAddress() { return new MailAddress("test.test.com"); }
+        public MailAddress GetOfflineReportReplyToAddress() { return new MailAddress("test.test.com"); }
+        public List<MailAddress> GetOfflineReportRecipients(string healthboard, string supplier) { return new List<MailAddress>(); }
+        public void RecordOfflineNotificationHasBeenSentForSite(string offlineSite) { }
+    }
+}
