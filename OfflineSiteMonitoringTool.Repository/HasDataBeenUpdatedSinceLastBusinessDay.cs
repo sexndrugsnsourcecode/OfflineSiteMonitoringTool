bd0e870b014ee2290315090c4bc907ba2c652615
Eric Blair <sexndrugsnsourcecode@gmail.com>
2012-03-15 12:26:09 +0000

    Got ExecuteDbQuery working. This means that I can delegate database calls and keep the exception catching and retrying logic seperate from the business logic of the queiries.

--- OfflineSiteMonitoringTool.Repository/HasDataBeenUpdatedSinceLastBusinessDay.cs
+++ OfflineSiteMonitoringTool.Repository/HasDataBeenUpdatedSinceLastBusinessDay.cs
@@ -8,7 +8,33 @@ namespace OfflineSiteMonitoringTool.Repository
 {
     public partial class Repository
     {
+        //public bool HasDataBeenUpdatedSinceLastBusinessDay(DateTime lastBusinessDay)
+        //{
+        //    DateTime lastestMsg = new DateTime();
+
+        //    lastestMsg = (from x in _reportingEntity.tbEPS_Msg
+        //                  orderby x.auditCreatedOn descending
+        //                  select x.auditCreatedOn).FirstOrDefault();
+
+        //    bool hasTableBeenUpdated;
+        //    if (lastestMsg > lastBusinessDay)
+        //        hasTableBeenUpdated = true;
+        //    else
+        //        hasTableBeenUpdated = false;
+
+        //    return hasTableBeenUpdated;
+        //}
+
         public bool HasDataBeenUpdatedSinceLastBusinessDay(DateTime lastBusinessDay)
+        {
+            bool hasTableBeenUpdated;
+
+            hasTableBeenUpdated = ExecuteDbQuery(hasDataBeenUpdatedSinceLastBusinessDay, lastBusinessDay);
+
+            return hasTableBeenUpdated;
+        }
+
+        private bool hasDataBeenUpdatedSinceLastBusinessDay(DateTime lastBusinessDay)
         {
             DateTime lastestMsg = new DateTime();
 
--- OfflineSiteMonitoringTool.Repository/Repository.cs
+++ OfflineSiteMonitoringTool.Repository/Repository.cs
@@ -11,10 +11,46 @@ namespace OfflineSiteMonitoringTool.Repository
     public partial class Repository : IRepository
     {
         private IReportingEntities _reportingEntity;
+        
+        // Load following from config file
+        int numberOfRetriesAfterDatabaseError = 5;
 
         public Repository(IReportingEntities reportingEntity)
         {
             _reportingEntity = reportingEntity;
+        }
+
+        private bool ExecuteDbQuery(Func<DateTime, bool> query, DateTime date)
+        {
+            int attempts = 0;
+
+            bool result;
+
+            while (true)
+            {
+                try
+                {
+                    result = query(date);
+
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    if (attempts <= numberOfRetriesAfterDatabaseError)
+                    {
+                        // Retry query
+                        attempts++;
+                        // _log.Add("Attempt: " + attempts + " : " + ex.Message);
+                        continue;
+                    }
+                    else
+                    {
+                        // Log error details and throw error
+                        // _log.Add("Max Attempts Reached : " + ex.Message);
+                        throw (ex);
+                    }
+                }
+            }
         }
 
         // Added dummy implementations of each method required by interface here to get solution to build
