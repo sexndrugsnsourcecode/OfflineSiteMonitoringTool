2db980c2c65d5beaf8474d0078628c530386c363
Eric Blair <sexndrugsnsourcecode@gmail.com>
2012-03-22 17:09:42 +0000

    Implemented logging for RecordOfflineNotificationHasBeenSentForSite

--- OfflineSiteMonitoringTool.Repository.Tests/RecordOfflineNotificationHasBeenSentForSiteTests.cs
+++ OfflineSiteMonitoringTool.Repository.Tests/RecordOfflineNotificationHasBeenSentForSiteTests.cs
@@ -45,15 +45,47 @@ namespace OfflineSiteMonitoringTool.Repository.Tests
         }
 
         [TestMethod]
-        public void RecordOfflineNotificationHasBeenSentForSite_SiteDoesNotExistInOfflineSitesTable_LogsError()
+        public void RecordOfflineNotificationHasBeenSentForSite_SiteDoesNotExistInOfflineSitesTable_LogsError_NothingAddedToTable()
         {
-            // TODO: Write this test once logging has been implemented
+            string orgId = "1111";
+
+            string expectedLogMessage = "ERROR: Tool has tried to record that an offline notification has been sent for site:" +
+                orgId + " which is not present in tbRPT_OfflineSites.";
+
+            // No need to add site details to offline sites table
+
+            repository.RecordOfflineNotificationHasBeenSentForSite(orgId);
+
+            int numberOfOfflineSites = (from x in mockReportingEntity.tbRPT_OfflineSites
+                                        select x).Count();
+
+            Assert.AreEqual(0, numberOfOfflineSites);
+            log.Verify(x => x.Add(expectedLogMessage));
         }
 
         [TestMethod]
         public void RecordOfflineNotificationHasBeenSentForSite_SiteAlreadyHasDateNotificationSentValueSent_DoesNotOverwriteValue_LogsError()
         {
-            // TODO: implement log error check once logging implemented
+            // Add site details to offline sites table
+            string orgId = "1111";
+            DateTime auditCreatedOn = DateTime.Now.AddDays(-3);
+            string supplier = "test";
+            string healthboard = "test";
+            DateTime dateOfflineNotificationSent = DateTime.Now.AddDays(-3);
+
+            mockReportingEntity.tbRPT_OfflineSites.AddObject(AddDataTo_tbRPT_OfflineSites.AddRow(orgId, auditCreatedOn, supplier, healthboard, dateOfflineNotificationSent));
+
+            repository.RecordOfflineNotificationHasBeenSentForSite(orgId);
+
+            string expectedLogMessage = "ERROR: Tool just attempted to record an offline notification having been sent for site: " + orgId +
+                " but offline notification has already been sent for this site.";
+
+            DateTime? dateOfflineNotificationValueAfterUpdateAttempt = (from x in mockReportingEntity.tbRPT_OfflineSites
+                                                                       where x.Org == orgId
+                                                                       select x.DateOfflineNotificationSent).FirstOrDefault();
+
+            Assert.AreEqual(dateOfflineNotificationSent, dateOfflineNotificationValueAfterUpdateAttempt);
+            log.Verify(x => x.Add(expectedLogMessage));
         }
     }
 }
--- OfflineSiteMonitoringTool.Repository/RecordOfflineNotificationHasBeenSentForSite.cs
+++ OfflineSiteMonitoringTool.Repository/RecordOfflineNotificationHasBeenSentForSite.cs
@@ -20,13 +20,24 @@ namespace OfflineSiteMonitoringTool.Repository
 
             if (site == null)
             {
-                //_log.Add("ERROR: Tried to update the email was sent for a non-existant organisation from tbRPT_IOfflineSites. Org: " + org);
+                _log.Add("ERROR: Tool has tried to record that an offline notification has been sent for site:" +
+                    offlineSite + " which is not present in tbRPT_OfflineSites.");
+
+                return;     
+            }
+
+            // Check that an offline notification has not already been sent to this site
+            if (site.DateOfflineNotificationSent != null)
+            {
+                _log.Add("ERROR: Tool just attempted to record an offline notification having been sent for site: " + site.Org +
+                    " but offline notification has already been sent for this site.");
+
                 return;
             }
 
             site.DateOfflineNotificationSent = DateTime.Now;
             _reportingEntity.SaveChanges();
-            //_log.Add("DB updated for: " + org);
+            
             return;
         }
     }
